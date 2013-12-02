c289f7c834b6a1eaa1773f4ae4368bf8eede9d76
tomgi <tomgillooly@trutest.co.nz>
2013-12-02 16:43:32 +1300

    Added tests to ensure that projections behave correctly in their interactions with zero length projections

--- tests/Projection_UnitTests.cpp
+++ tests/Projection_UnitTests.cpp
@@ -110,3 +110,17 @@ TEST_F(ProjectionTests, OverlapWhenOneLaysDirectlyOnTheOther)
 
 	ASSERT_TRUE(p1.OverlapsWith(p2));
 }
+
+TEST_F(ProjectionTests, OverlapsWithZeroLengthProjection)
+{
+    Projection p1(-1, 1), p2(0, 0);
+    ASSERT_TRUE(p1.OverlapsWith(p2));    
+}
+
+TEST_F(ProjectionTests, NonOverlapWithZeroLengthProjAtEitherEnd)
+{
+    Projection p1(-1, 1), p2(-1, -1);
+    ASSERT_FALSE(p1.OverlapsWith(p2));
+    p2 = Projection(1, 1);
+    ASSERT_FALSE(p1.OverlapsWith(p2));
+}
